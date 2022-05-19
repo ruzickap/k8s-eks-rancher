773ebb0388f69663a8dbddb0a89b0558dfd5d137
Petr Ruzicka <petr.ruzicka@gmail.com>
2022-05-19 23:05:16 +0200

    Improve renovate, add rancher + cert-manager (#18)

--- docker-create-k8s-eks-rancher.sh
+++ docker-create-k8s-eks-rancher.sh
@@ -6,6 +6,7 @@ docker run -it --rm \
   -e AWS_ACCESS_KEY_ID \
   -e AWS_SECRET_ACCESS_KEY \
   -e AWS_SESSION_TOKEN \
+  -e MY_PASSWORD \
   -v "${PWD}:/mnt" \
   -w /mnt \
   ubuntu ./create-k8s-eks-rancher.sh
